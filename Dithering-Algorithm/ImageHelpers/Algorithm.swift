77dbf534ecbabfc243b030c1c267e599704a0f5d
mannylopez <manuel.est.lopez@gmail.com>
2025-04-19 23:08:24 -0700

    Add Jarvis algo

--- Dithering-Algorithm/ImageHelpers/Algorithm.swift
+++ Dithering-Algorithm/ImageHelpers/Algorithm.swift
@@ -14,6 +14,7 @@ enum Algorithm: String, CaseIterable {
   case atkinson = "Atkinson"
   case aman = "Aman"
   case floydSteinberg = "Floyd-Steinberg"
+  case jarvisJudiceNinke = "Jarvis, Judice, and Ninke"
   
   func rgbaMatrix(for image: StockImage = .catFullColor) -> [[RGBA]] {
     switch self {
@@ -29,9 +30,12 @@ enum Algorithm: String, CaseIterable {
       PixelReader.amanRGBA(from: image)
     case .floydSteinberg:
       PixelReader.floydSteinbergRGBA(from: image)
+    case .jarvisJudiceNinke:
+      PixelReader.createRGBA(from: image, using: self)
     }
   }
   
+  // Found in https://raw.githubusercontent.com/cyotek/Dithering/master/resources/DHALF.TXT
   func pattern() -> [DitherPixel] {
     switch self {
     case .copy:
@@ -41,11 +45,11 @@ enum Algorithm: String, CaseIterable {
     case .thresholding:
       []
     case .atkinson:
-      //      X   1   1
+      //      *   1   1
       //  1   1   1
       //      1
       //
-      //    (1/8)
+      //  (1/8)
       [
         DitherPixel(y: 0, x: 1, weight: 0.125),
         DitherPixel(y: 0, x: 2, weight: 0.125),
@@ -64,15 +68,35 @@ enum Algorithm: String, CaseIterable {
         DitherPixel(y: 2, x: 0, weight: 0.0625),
       ]
     case .floydSteinberg:
-      //      X   7
+      //      *   7
       //  3   5   1
       //
-      //    (1/16)
+      //  (1/16)
       [
         DitherPixel(y: 0, x: 1, weight: 7.0 / 16.0),
         DitherPixel(y: 1, x: -1, weight: 3.0 / 16.0),
         DitherPixel(y: 1, x: 0, weight: 5.0 / 16.0),
         DitherPixel(y: 1, x: 1, weight: 1.0 / 16.0),
+      ]
+    case .jarvisJudiceNinke:
+      //          *   7   5
+      //  3   5   7   5   3
+      //  1   3   5   3   1
+      //
+      //  (1/48)
+      [
+        DitherPixel(y: 0, x: 1, weight: 7.0 / 48.0),
+        DitherPixel(y: 0, x: 2, weight: 5.0 / 48.0),
+        DitherPixel(y: 1, x: -2, weight: 3.0 / 48.0),
+        DitherPixel(y: 1, x: -1, weight: 5.0 / 48.0),
+        DitherPixel(y: 1, x: 0, weight: 7.0 / 48.0),
+        DitherPixel(y: 1, x: 1, weight: 5.0 / 48.0),
+        DitherPixel(y: 1, x: 2, weight: 3.0 / 48.0),
+        DitherPixel(y: 2, x: -2, weight: 1.0 / 48.0),
+        DitherPixel(y: 2, x: -1, weight: 3.0 / 48.0),
+        DitherPixel(y: 2, x: 0, weight: 4.0 / 48.0),
+        DitherPixel(y: 2, x: 1, weight: 3.0 / 48.0),
+        DitherPixel(y: 2, x: 2, weight: 1.0 / 48.0),
       ]
     }
   }
